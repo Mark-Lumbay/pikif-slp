15c86fe07abd3cda8148846a5ed508a6556af71e
MarkLumbay <20180013531@my.xu.edu.ph>
2023-04-12 00:26:40 +0800

    Created a partial scaffold for the API and the server

---
+++ config.js
@@ -0,0 +1,31 @@
+"use strict";
+
+const dotenv = require("dotenv");
+
+dotenv.config();
+
+const {
+  PORT,
+  HOST,
+  HOST_URL,
+  API_KEY,
+  AUTH_DOMAIN,
+  PROJECT_ID,
+  STORAGE_BUCKET,
+  MESSAGING_SENDER_ID,
+  APP_ID,
+} = process.env;
+
+module.exports = {
+  port: PORT,
+  host: HOST,
+  url: HOST_URL,
+  firebaseConfig: {
+    apiKey: API_KEY,
+    authDomain: AUTH_DOMAIN,
+    projectId: PROJECT_ID,
+    storageBucket: STORAGE_BUCKET,
+    messagingSenderId: MESSAGING_SENDER_ID,
+    appId: APP_ID,
+  },
+};
---
+++ index.js
@@ -0,0 +1,16 @@
+"use strict";
+
+const express = require("express");
+const cors = require("cors");
+const bodyParser = require("body-parser");
+const config = require("./config");
+
+const app = express();
+
+app.use(express.json());
+app.use(cors());
+app.use(bodyParser.json());
+
+app.listen(config.port, () => {
+  console.log(`Listening on url http://localhost${config.port}`);
+});

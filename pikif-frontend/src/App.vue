c0959872f6f4d3148afb39772e73fee0abba1d41
MarkLumbay <20180013531@my.xu.edu.ph>
2023-04-13 14:29:31 +0800

    installed tailwind css to the scaffold

--- pikif-frontend/src/App.vue
+++ pikif-frontend/src/App.vue
@@ -1,26 +1,14 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <h1>Test</h1>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import "./assets/tailwind.css";
 
 export default {
-  name: 'App',
-  components: {
-    HelloWorld
-  }
-}
+  name: "App",
+  components: {},
+};
 </script>
 
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-</style>
+<style></style>

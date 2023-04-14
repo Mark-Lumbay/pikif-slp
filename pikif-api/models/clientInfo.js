654236f637309136f64e872d6a1a4239bae67f85
MarkLumbay <20180013531@my.xu.edu.ph>
2023-04-15 00:52:43 +0800

    added schema for validation of client information

--- pikif-api/models/clientInfo.js
+++ pikif-api/models/clientInfo.js
@@ -1,8 +1,65 @@
 import Database from "../database/database.js";
+import Joi from "joi";
 
 class ClientModel {
   addInfo(data) {
     // Perform checks here before creating
+    const clientInfoSchema = Joi.object({
+      clientInfo: Joi.object({
+        interviewDate: Joi.string().required(),
+        firstName: Joi.string().required(),
+        middleName: Joi.string().required(),
+        lastName: Joi.string().required(),
+        status: Joi.string().required(),
+        age: Joi.number().required(),
+        sex: Joi.string().required(),
+        address: Joi.string().required(),
+        birthDate: Joi.string().required(),
+        birthPlace: Joi.string().required(),
+        religion: Joi.string().required(),
+        contactNum: Joi.string().required(),
+        educAttn: Joi.string().required(),
+        categoryObj: Joi.array().required(),
+        condition: Joi.string().required(),
+        materials: Joi.object({
+          roof: Joi.string().required(),
+          walls: Joi.string().required(),
+          floor: Joi.string().required(),
+        }).required(),
+        appliances: Joi.string().required(),
+      }).required(),
+      informantInfo: Joi.object({
+        interviewDate: Joi.string().required(),
+        firstName: Joi.string().required(),
+        middleName: Joi.string().required(),
+        lastName: Joi.string().required(),
+        status: Joi.string().required(),
+        age: Joi.number().required(),
+        sex: Joi.string().required(),
+        address: Joi.string().required(),
+        birthDate: Joi.string().required(),
+        birthPlace: Joi.string().required(),
+        religion: Joi.string().required(),
+        contactNum: Joi.string().required(),
+        occupation: Joi.string().required(),
+        income: Joi.object({
+          type: Joi.string().required(),
+          amount: Joi.string().required(),
+        }).required(),
+        employment: Joi.string().required(),
+        employmentStat: Joi.string().required(),
+        employerName: Joi.string().required(),
+        workAdd: Joi.string().required(),
+        assistance: Joi.string().required(),
+        otherInc: Joi.string().required(),
+        monthlyInc: Joi.string().required(),
+        probs: Joi.string().required(),
+      }).required(),
+      initialFindings: Joi.object({
+        findings: Joi.string().required(),
+        date: Joi.string().required(),
+      }).required(),
+    });
     return Database.addClientInfo("clientInfo", data);
   }
 }

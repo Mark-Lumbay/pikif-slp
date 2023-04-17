6780e77e8d97572bd2e33c7e4519da6abed3e17d
MarkLumbay <20180013531@my.xu.edu.ph>
2023-04-17 17:41:24 +0800

    modified the login page. Partially complete

--- pikif-app/src/views/LogIn.vue
+++ pikif-app/src/views/LogIn.vue
@@ -1,47 +1,61 @@
 <template>
-  <div class="w-full h-full justify-center items-center flex">
-    <div class="outline-2">
-      <form class="" @submit.prevent="loginUser">
-        <div class="mb-2 space-y-2">
-          <label class="block font-semibold" for="email">Email</label>
-          <input
-            class="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
-            name="email"
-            placeholder="Enter Email"
-            v-model="email"
-          />
-        </div>
+  <div class="w-full h-full flex flex-row items-center justify-center">
+    <div
+      class="w-[55%] h-full bg-[url('../assets/bg.png')] bg-center bg-no-repeat bg-cover"
+    ></div>
 
-        <div class="mb-2 space-y-2">
-          <label class="block font-semibold" for="email">Password</label>
-          <input
-            class="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
-            name="email"
-            placeholder="Enter Password"
-            v-model="password"
-          />
+    <div class="w-full h-full">
+      <div class="w-full h-[25%] bg-blue-900"></div>
+      <div class="w-full px-56 py-12">
+        <div class="mb-12 w-full text-center">
+          <h1 class="text-4xl font-semibold">LOGIN</h1>
         </div>
+        <div class="outline-2">
+          <form class="" @submit.prevent="loginUser">
+            <div class="mb-6 space-y-2">
+              <label class="block font-semibold" for="email">Email</label>
+              <input
+                class="shadow appearance-none border rounded w-full h-14 py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
+                name="email"
+                type="email"
+                placeholder="Enter Email"
+                v-model="email"
+              />
+            </div>
 
-        <div class="mb-2 space-y-2 mt-4">
-          <a href="#" class="text-xs underline">Forgot Password?</a>
+            <div class="mb-2 space-y-2">
+              <label class="block font-semibold" for="email">Password</label>
+              <input
+                class="shadow appearance-none border rounded w-full h-14 py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
+                name="email"
+                type="password"
+                placeholder="Enter Password"
+                v-model="password"
+              />
+            </div>
+
+            <div class="mb-2 space-y-2 mt-4 w-full text-right">
+              <a href="#" class="text-s underline">Forgot Password?</a>
+            </div>
+
+            <div class="mb-2 space-y-2 mt-14">
+              <button
+                class="bg-primaryBtn mb-2 hover:bg-primaryHovBtn text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline w-full h-14 transition-all ease-in-out"
+                @click.prevent="loginUser"
+              >
+                Login
+              </button>
+
+              <button
+                class="bg-primaryBtn mb-2 hover:bg-primaryHovBtn text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline w-full h-14 transition-all ease-in-out"
+                @click.prevent="test"
+              >
+                Sign Up
+              </button>
+            </div>
+          </form>
         </div>
-
-        <div class="mb-2 space-y-2 mt-8">
-          <button
-            class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline w-full transition-all ease-in-out"
-            @click.prevent="loginUser"
-          >
-            Login
-          </button>
-
-          <button
-            class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline w-full transition-all ease-in-out"
-            @click.prevent="test"
-          >
-            Test
-          </button>
-        </div>
-      </form>
+      </div>
     </div>
   </div>
 </template>
--- pikif-app/tailwind.config.js
+++ pikif-app/tailwind.config.js
@@ -1,9 +1,13 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ['./public/**/*.html', './src/**/*.{vue,js,ts,jsx,tsx}'],
+  content: ["./public/**/*.html", "./src/**/*.{vue,js,ts,jsx,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        primaryBtn: "#236576",
+        primaryHovBtn: "#1c515e",
+      },
+    },
   },
   plugins: [],
-}
-
+};

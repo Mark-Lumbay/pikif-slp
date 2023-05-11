f4b1b2f3e96401a9aed6997f3ed055732a45e022
MarkLumbay <20180013531@my.xu.edu.ph>
2023-05-11 12:01:39 +0800

    created house materials field

--- pikif-quasar/src/components/AddClient.vue
+++ pikif-quasar/src/components/AddClient.vue
@@ -381,7 +381,7 @@
               </div>
             </div>
           </div>
-          <div class="w-full md:w-1/6 px-3 mb-6 md:mb-0">
+          <div class="w-full md:w-1/6 px-3 mb-6 md:mb-0 border-r-2">
             <label
               class="block uppercase tracking-wide text-gray-700 text-xs font-bold mb-2"
               for="grid-roof-others"
@@ -394,6 +394,109 @@
               type="text"
               placeholder="Other options"
               :disabled="roofMats != 'Others'"
+              v-model="roofOthers"
+            />
+          </div>
+
+          <div class="w-full md:w-1/6 px-3 mb-6 md:mb-0">
+            <label
+              class="block uppercase tracking-wide text-gray-700 text-xs font-bold mb-2"
+              for="grid-wall"
+            >
+              Wall Material
+            </label>
+            <div class="relative">
+              <select
+                class="block appearance-none w-full bg-gray-200 border border-gray-200 text-gray-700 py-3 px-4 pr-8 rounded leading-tight focus:outline-none focus:bg-white focus:border-gray-500"
+                id="grid-wall"
+                v-model="wallMats"
+              >
+                <option value="Nipa">Nipa</option>
+                <option value="Bamboo">Bamboo</option>
+                <option value="Wood">Wood</option>
+                <option value="Concrete">Concrete</option>
+                <option value="Others">Others</option>
+              </select>
+              <div
+                class="pointer-events-none absolute inset-y-0 right-0 flex items-center px-2 text-gray-700"
+              >
+                <svg
+                  class="fill-current h-4 w-4"
+                  xmlns="http://www.w3.org/2000/svg"
+                  viewBox="0 0 20 20"
+                >
+                  <path
+                    d="M9.293 12.95l.707.707L15.657 8l-1.414-1.414L10 10.828 5.757 6.586 4.343 8z"
+                  />
+                </svg>
+              </div>
+            </div>
+          </div>
+          <div class="w-full md:w-1/6 px-3 mb-6 md:mb-0 border-r-2">
+            <label
+              class="block uppercase tracking-wide text-gray-700 text-xs font-bold mb-2"
+              for="grid-roof-others"
+            >
+              Others:
+            </label>
+            <input
+              class="appearance-none block w-full bg-gray-200 text-gray-700 border rounded py-3 px-4 mb-3 leading-tight focus:outline-none focus:bg-white focus:border-gray-500"
+              id="grid-roof-others"
+              type="text"
+              placeholder="Other options"
+              :disabled="wallMats != 'Others'"
+              v-model="wallOthers"
+            />
+          </div>
+
+          <div class="w-full md:w-1/6 px-3 mb-6 md:mb-0">
+            <label
+              class="block uppercase tracking-wide text-gray-700 text-xs font-bold mb-2"
+              for="grid-wall"
+            >
+              Floor Material
+            </label>
+            <div class="relative">
+              <select
+                class="block appearance-none w-full bg-gray-200 border border-gray-200 text-gray-700 py-3 px-4 pr-8 rounded leading-tight focus:outline-none focus:bg-white focus:border-gray-500"
+                id="grid-wall"
+                v-model="floorMats"
+              >
+                <option value="Soil">Soil</option>
+                <option value="Bamboo">Bamboo</option>
+                <option value="Wood">Wood</option>
+                <option value="Concrete">Concrete</option>
+                <option value="Others">Others</option>
+              </select>
+              <div
+                class="pointer-events-none absolute inset-y-0 right-0 flex items-center px-2 text-gray-700"
+              >
+                <svg
+                  class="fill-current h-4 w-4"
+                  xmlns="http://www.w3.org/2000/svg"
+                  viewBox="0 0 20 20"
+                >
+                  <path
+                    d="M9.293 12.95l.707.707L15.657 8l-1.414-1.414L10 10.828 5.757 6.586 4.343 8z"
+                  />
+                </svg>
+              </div>
+            </div>
+          </div>
+          <div class="w-full md:w-1/6 px-3 mb-6 md:mb-0 border-r-2">
+            <label
+              class="block uppercase tracking-wide text-gray-700 text-xs font-bold mb-2"
+              for="grid-roof-others"
+            >
+              Others:
+            </label>
+            <input
+              class="appearance-none block w-full bg-gray-200 text-gray-700 border rounded py-3 px-4 mb-3 leading-tight focus:outline-none focus:bg-white focus:border-gray-500"
+              id="grid-roof-others"
+              type="text"
+              placeholder="Other options"
+              :disabled="floorMats != 'Others'"
+              v-model="floorOthers"
             />
           </div>
         </div>
@@ -414,32 +517,26 @@ export default {
     };
 
     const housingOthers = ref("");
-
     const housingCond = ref("Squater's Shanty");
+
     const roofMats = ref("Nipa");
+    const roofOthers = ref("");
 
-    const others = computed(() => {
-      if (housingCond.value != "Others") {
-        return false;
-      } else {
-        return true;
-      }
-    });
+    const wallMats = ref("Nipa");
+    const wallOthers = ref("");
 
-    const roofOthers = computed(() => {
-      if (roofMats.value != "Others") {
-        return false;
-      } else {
-        return true;
-      }
-    });
+    const floorMats = ref("Soil");
+    const floorOthers = ref("");
 
     return {
       housingOthers,
       housingCond,
       roofMats,
       roofOthers,
-      others,
+      wallMats,
+      wallOthers,
+      floorMats,
+      floorOthers,
     };
   },
 };

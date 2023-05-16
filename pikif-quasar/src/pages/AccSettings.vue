daed5e2b1b2c25bbc79f14a0c67bf7e9ef140f4e
MarkLumbay <20180013531@my.xu.edu.ph>
2023-05-16 10:41:50 +0800

    finished creating account settings UI

--- pikif-quasar/src/pages/AccSettings.vue
+++ pikif-quasar/src/pages/AccSettings.vue
@@ -11,7 +11,7 @@
         </div>
 
         <form>
-          <div class="flex flex-wrap -mx-3 w-full px-6 mt-6 mb-1">
+          <div class="flex flex-wrap w-full px-6 mt-6 mb-1">
             <div class="w-[50%]">
               <p
                 class="block tracking-wide text-gray-700 font-bold mb-2 text-lg"
@@ -20,7 +20,7 @@
               </p>
             </div>
 
-            <div class="flex justify-end items-center w-[50%]">
+            <div class="flex justify-end w-[50%]">
               <button
                 class="text-primaryRed hover:text-white hover:bg-primaryRed hover:border-transparent font-semibold py-2 px-4 border border-primaryRed rounded"
                 v-if="!editMode"
@@ -44,8 +44,8 @@
             </div>
           </div>
 
-          <div class="flex flex-wrap -mx-3 mb-4 px-6">
-            <div class="w-full md:w-1/2 mb-6 md:mb-0">
+          <div class="flex w-full mb-4 px-6">
+            <div class="w-full md:w-1/2 pr-4 mb-6 md:mb-0">
               <label
                 class="block uppercase tracking-wide text-gray-700 text-xs font-bold mb-2"
                 for="grid-first-name"
@@ -58,11 +58,8 @@
                 type="text"
                 placeholder="First Name"
               />
-              <!-- <p class="text-red-500 text-xs italic">
-              Please fill out this field.
-            </p> -->
             </div>
-            <div class="w-full md:w-1/2 px-3">
+            <div class="w-full md:w-1/2 pl-4">
               <label
                 class="block uppercase tracking-wide text-gray-700 text-xs font-bold mb-2"
                 for="grid-last-name"
@@ -78,65 +75,56 @@
             </div>
           </div>
 
-          <div class="flex flex-wrap -mx-3 w-full justify-start px-6 mt-6 mb-1">
-            <div class="w-[50%]">
+          <!-- Email and Password -->
+
+          <div class="flex flex-wrap w-full px-6 mt-6 mb-1">
+            <div class="w-full">
               <p
                 class="block tracking-wide text-gray-700 font-bold mb-2 text-lg"
               >
                 Credentials
               </p>
             </div>
-
-            <div class="flex justify-end items-center w-[50%]">
-              <button
-                class="text-primaryRed hover:text-white hover:bg-primaryRed hover:border-transparent font-semibold py-2 px-4 border border-primaryRed rounded"
-                v-if="!editMode"
-              >
-                Edit
-              </button>
-
-              <button
-                class="text-primaryBtn hover:text-white hover:bg-btnGreen hover:border-transparent font-semibold py-2 px-4 border border-primaryBtn rounded"
-                v-if="editMode"
-              >
-                Cancel
-              </button>
-
-              <button
-                class="text-btnGreen hover:text-white hover:bg-btnGreen hover:border-transparent font-semibold py-2 px-4 border border-btnGreen rounded"
-                v-if="editMode"
-              >
-                Save
-              </button>
-            </div>
           </div>
 
-          <div class="flex flex-wrap -mx-3 mb-4 px-6">
-            <div class="w-full md:w-1/2 mb-6 md:mb-0">
-              <label
-                class="block uppercase tracking-wide text-gray-700 text-xs font-bold mb-2"
-                for="grid-femail"
-              >
-                Email
-              </label>
-              <input
-                class="appearance-none block w-full bg-gray-200 text-gray-700 border focus:border-gray-500 rounded py-3 px-4 mb-3 leading-tight focus:outline-none focus:bg-white"
-                id="grid-email"
-                type="email"
-              />
+          <div class="flex w-full mb-4 px-6">
+            <div class="w-[50%] flex flex-row pr-4 mb-6 md:mb-0">
+              <div class="flex flex-col w-[50%]">
+                <label
+                  class="block uppercase tracking-wide text-gray-700 text-xs font-bold mb-2"
+                  for="grid-email"
+                >
+                  Email
+                </label>
+                <p>lumbaymark@gmail.com</p>
+              </div>
+              <div class="flex justify-end w-[50%]">
+                <button
+                  class="text-primaryRed h-12 hover:text-white hover:bg-primaryRed hover:border-transparent font-semibold py-2 px-6 border border-primaryRed rounded"
+                  v-if="!editMode"
+                >
+                  Change
+                </button>
+              </div>
             </div>
-            <div class="w-full md:w-1/2 px-3">
-              <label
-                class="block uppercase tracking-wide text-gray-700 text-xs font-bold mb-2"
-                for="grid-password"
-              >
-                Password
-              </label>
-              <input
-                class="appearance-none block w-full bg-gray-200 text-gray-700 border border-gray-200 rounded py-3 px-4 leading-tight focus:outline-none focus:bg-white focus:border-gray-500"
-                id="grid-password"
-                type="password"
-              />
+
+            <div class="w-[50%] flex flex-row pl-4 mb-6 md:mb-0">
+              <div class="flex flex-col w-[50%]">
+                <label
+                  class="block uppercase tracking-wide text-gray-700 text-xs font-bold mb-2"
+                  for="grid-email"
+                >
+                  Password
+                </label>
+              </div>
+              <div class="flex justify-end w-[50%]">
+                <button
+                  class="text-primaryRed h-12 hover:text-white hover:bg-primaryRed hover:border-transparent font-semibold py-2 px-6 border border-primaryRed rounded"
+                  v-if="!editMode"
+                >
+                  Change
+                </button>
+              </div>
             </div>
           </div>
         </form>
@@ -146,12 +134,19 @@
 </template>
 
 <script>
-import { ref } from "vue";
+import { ref, onMounted } from "vue";
+import store from "../store";
 
 export default {
   setup() {
     const editMode = ref(false);
     const readOnly = ref(true);
+    const userData = ref({
+      firstName: "",
+      lastName: "",
+    });
+
+    onMounted(() => {});
 
     return {
       editMode,

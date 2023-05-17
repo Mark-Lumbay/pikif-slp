5ee32765393dace31ef796ae8c6a93e9f2ddfb5b
MarkLumbay <20180013531@my.xu.edu.ph>
2023-05-17 17:37:49 +0800

    fixed auth issue

--- pikif-quasar/src/router/middleware.js
+++ pikif-quasar/src/router/middleware.js
@@ -1,35 +1,48 @@
 import store from "../store";
 import { auth } from "../firebase.js";
-
 let user = store.getters.getState;
 
 function getAuthState() {
   return new Promise((resolve) => {
     auth.onAuthStateChanged((newUser) => {
-      resolve(newUser.getIdTokenResult());
+      console.log("CALLED");
+      console.log(newUser);
+      if (!newUser) {
+        resolve(newUser);
+      } else {
+        resolve(newUser.getIdTokenResult());
+      }
     });
   });
 }
 
 async function runCheck() {
   user = await getAuthState();
-  const userDetails = {
-    fName: user.claims.firstName,
-    lName: user.claims.lastName,
-    user: user,
-    email: user.claims.email,
-    uid: user.claims.user_id,
-  };
+  if (!user) {
+    user = await getAuthState();
+  } else {
+    const userDetails = {
+      fName: user.claims.firstName,
+      lName: user.claims.lastName,
+      user: user,
+      email: user.claims.email,
+      uid: user.claims.user_id,
+    };
 
-  console.log(user);
-  store.dispatch("storeUser", userDetails);
+    console.log(user);
+    store.dispatch("storeUser", userDetails);
+  }
 }
 
 export default async function middleware(to, from, next) {
-  await runCheck();
-  if (to.meta.requiresAuth && !user) {
-    return next("/login");
-  } else {
+  if (!to.meta.requiresAuth) {
     return next();
+  } else {
+    await runCheck();
+    if (to.meta.requiresAuth && !user) {
+      return next("/login");
+    } else {
+      return next();
+    }
   }
 }

e188f68550af87449a8a467fd3a6f1fe30a91224
Kit Francis E. Sajulga <20180014673@my.xu.edu.ph>
2023-05-17 14:49:10 +0800

    committt

--- pikif-quasar/src/pages/HomePage.vue
+++ pikif-quasar/src/pages/HomePage.vue
@@ -1,75 +1,5 @@
 <template>
-
   <div class="flex space-y-4">
-<!-- Modal -->
-<div
-  data-te-modal-init
-  class="fixed left-0 top-0 z-[1055] hidden h-full w-full overflow-y-auto overflow-x-hidden outline-none"
-  id="staticBackdrop"
-  data-te-backdrop="static"
-  data-te-keyboard="false"
-  tabindex="-1"
-  aria-labelledby="staticBackdropLabel"
-  aria-hidden="true">
-  <div
-    data-te-modal-dialog-ref
-    class="pointer-events-none relative w-auto translate-y-[-50px] opacity-0 transition-all duration-300 ease-in-out min-[576px]:mx-auto min-[576px]:mt-7 min-[576px]:max-w-[500px]">
-    <div
-      class="min-[576px]:shadow-[0_0.5rem_1rem_rgba(#000, 0.15)] pointer-events-auto relative flex w-full flex-col rounded-md border-none bg-white bg-clip-padding text-current shadow-lg outline-none dark:bg-neutral-600">
-      <div
-        class="flex flex-shrink-0 items-center justify-between rounded-t-md border-b-2 border-neutral-100 border-opacity-100 p-4 dark:border-opacity-50">
-        <!--Modal title-->
-        <h5
-          class="text-xl font-medium leading-normal text-neutral-800 dark:text-neutral-200"
-          id="exampleModalLabel">
-          Modal title
-        </h5>
-        <!--Close button-->
-        <button
-          type="button"
-          class="box-content rounded-none border-none hover:no-underline hover:opacity-75 focus:opacity-100 focus:shadow-none focus:outline-none"
-          data-te-modal-dismiss
-          aria-label="Close">
-          <svg
-            xmlns="http://www.w3.org/2000/svg"
-            fill="none"
-            viewBox="0 0 24 24"
-            stroke-width="1.5"
-            stroke="currentColor"
-            class="h-6 w-6">
-            <path
-              stroke-linecap="round"
-              stroke-linejoin="round"
-              d="M6 18L18 6M6 6l12 12" />
-          </svg>
-        </button>
-      </div>
-
-      <!--Modal body-->
-      <div data-te-modal-body-ref class="relative p-4">...</div>
-
-      <!--Modal footer-->
-      <div
-        class="flex flex-shrink-0 flex-wrap items-center justify-end rounded-b-md border-t-2 border-neutral-100 border-opacity-100 p-4 dark:border-opacity-50">
-        <button
-          type="button"
-          class="inline-block rounded bg-primary-100 px-6 pb-2 pt-2.5 text-xs font-medium uppercase leading-normal text-primary-700 transition duration-150 ease-in-out hover:bg-primary-accent-100 focus:bg-primary-accent-100 focus:outline-none focus:ring-0 active:bg-primary-accent-200"
-          data-te-modal-dismiss
-          data-te-ripple-init
-          data-te-ripple-color="light">
-          Close
-        </button>
-        <button
-          type="button"
-          class="ml-1 inline-block rounded bg-primary px-6 pb-2 pt-2.5 text-xs font-medium uppercase leading-normal text-white shadow-[0_4px_9px_-4px_#3b71ca] transition duration-150 ease-in-out hover:bg-primary-600 hover:shadow-[0_8px_9px_-4px_rgba(59,113,202,0.3),0_4px_18px_0_rgba(59,113,202,0.2)] focus:bg-primary-600 focus:shadow-[0_8px_9px_-4px_rgba(59,113,202,0.3),0_4px_18px_0_rgba(59,113,202,0.2)] focus:outline-none focus:ring-0 active:bg-primary-700 active:shadow-[0_8px_9px_-4px_rgba(59,113,202,0.3),0_4px_18px_0_rgba(59,113,202,0.2)] dark:shadow-[0_4px_9px_-4px_rgba(59,113,202,0.5)] dark:hover:shadow-[0_8px_9px_-4px_rgba(59,113,202,0.2),0_4px_18px_0_rgba(59,113,202,0.1)] dark:focus:shadow-[0_8px_9px_-4px_rgba(59,113,202,0.2),0_4px_18px_0_rgba(59,113,202,0.1)] dark:active:shadow-[0_8px_9px_-4px_rgba(59,113,202,0.2),0_4px_18px_0_rgba(59,113,202,0.1)]"
-          data-te-ripple-init
-          data-te-ripple-color="light">
-          Understood
-        </button>
-      </div>
-    </div>
-  </div>
-</div>
     <div
       class="flex w-full h-[10vh] bg-white shadow-md sm:rounded-lg items-center px-6"
     >
@@ -246,19 +176,6 @@
         </button>
       </div>
     </div>
-<<<<<<< HEAD
-    <div>
-
-     <button class="px-4 py-2 bg-primaryRed text-white text-base font-medium rounded-md w-full shadow-sm hover:bg-primaryRedHover focus:outline-none focus:ring-2 focus:ring-green-300"
-     data-te-toggle="modal"
-  data-te-target="#staticBackdrop"
-  data-te-ripple-init
-  data-te-ripple-color="light"
-     >Generate Report</button>
-    </div>
-  </div>
-=======
->>>>>>> 0b9c55a259bea90e08ee710493c9da1e5dc85bba
   </div>
 </template>
 
@@ -458,6 +375,443 @@ export default {
       viewRow,
       firstName,
       getName,
+    };
+  },
+};
+</script>
+<template>
+
+  <div class="flex space-y-4">
+<!-- Modal -->
+<div
+  data-te-modal-init
+  class="fixed left-0 top-0 z-[1055] hidden h-full w-full overflow-y-auto overflow-x-hidden outline-none"
+  id="staticBackdrop"
+  data-te-backdrop="static"
+  data-te-keyboard="false"
+  tabindex="-1"
+  aria-labelledby="staticBackdropLabel"
+  aria-hidden="true">
+  <div
+    data-te-modal-dialog-ref
+    class="pointer-events-none relative w-auto translate-y-[-50px] opacity-0 transition-all duration-300 ease-in-out min-[576px]:mx-auto min-[576px]:mt-7 min-[576px]:max-w-[500px]">
+    <div
+      class="min-[576px]:shadow-[0_0.5rem_1rem_rgba(#000, 0.15)] pointer-events-auto relative flex w-full flex-col rounded-md border-none bg-white bg-clip-padding text-current shadow-lg outline-none dark:bg-neutral-600">
+      <div
+        class="flex flex-shrink-0 items-center justify-between rounded-t-md border-b-2 border-neutral-100 border-opacity-100 p-4 dark:border-opacity-50">
+        <!--Modal title-->
+        <h5
+          class="text-xl font-medium leading-normal text-neutral-800 dark:text-neutral-200"
+          id="exampleModalLabel">
+          Modal title
+        </h5>
+        <!--Close button-->
+        <button
+          type="button"
+          class="box-content rounded-none border-none hover:no-underline hover:opacity-75 focus:opacity-100 focus:shadow-none focus:outline-none"
+          data-te-modal-dismiss
+          aria-label="Close">
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            fill="none"
+            viewBox="0 0 24 24"
+            stroke-width="1.5"
+            stroke="currentColor"
+            class="h-6 w-6">
+            <path
+              stroke-linecap="round"
+              stroke-linejoin="round"
+              d="M6 18L18 6M6 6l12 12" />
+          </svg>
+        </button>
+      </div>
+
+      <!--Modal body-->
+      <div data-te-modal-body-ref class="relative p-4">...</div>
+
+      <!--Modal footer-->
+      <div
+        class="flex flex-shrink-0 flex-wrap items-center justify-end rounded-b-md border-t-2 border-neutral-100 border-opacity-100 p-4 dark:border-opacity-50">
+        <button
+          type="button"
+          class="inline-block rounded bg-primary-100 px-6 pb-2 pt-2.5 text-xs font-medium uppercase leading-normal text-primary-700 transition duration-150 ease-in-out hover:bg-primary-accent-100 focus:bg-primary-accent-100 focus:outline-none focus:ring-0 active:bg-primary-accent-200"
+          data-te-modal-dismiss
+          data-te-ripple-init
+          data-te-ripple-color="light">
+          Close
+        </button>
+        <button
+          type="button"
+          class="ml-1 inline-block rounded bg-primary px-6 pb-2 pt-2.5 text-xs font-medium uppercase leading-normal text-white shadow-[0_4px_9px_-4px_#3b71ca] transition duration-150 ease-in-out hover:bg-primary-600 hover:shadow-[0_8px_9px_-4px_rgba(59,113,202,0.3),0_4px_18px_0_rgba(59,113,202,0.2)] focus:bg-primary-600 focus:shadow-[0_8px_9px_-4px_rgba(59,113,202,0.3),0_4px_18px_0_rgba(59,113,202,0.2)] focus:outline-none focus:ring-0 active:bg-primary-700 active:shadow-[0_8px_9px_-4px_rgba(59,113,202,0.3),0_4px_18px_0_rgba(59,113,202,0.2)] dark:shadow-[0_4px_9px_-4px_rgba(59,113,202,0.5)] dark:hover:shadow-[0_8px_9px_-4px_rgba(59,113,202,0.2),0_4px_18px_0_rgba(59,113,202,0.1)] dark:focus:shadow-[0_8px_9px_-4px_rgba(59,113,202,0.2),0_4px_18px_0_rgba(59,113,202,0.1)] dark:active:shadow-[0_8px_9px_-4px_rgba(59,113,202,0.2),0_4px_18px_0_rgba(59,113,202,0.1)]"
+          data-te-ripple-init
+          data-te-ripple-color="light">
+          Understood
+        </button>
+      </div>
+    </div>
+  </div>
+</div>
+    <div
+      class="flex w-full h-[10vh] bg-white shadow-md sm:rounded-lg items-center px-6"
+    >
+      <div class="flex w-[50%]">
+        <p class="font-semibold text-2xl">
+          Welcome back, {{ props.userName }}!
+        </p>
+      </div>
+      <div class="flex w-[50%] justify-end">
+        <p class="text-xl">Monday, May 8, 2023</p>
+      </div>
+
+      <div class="flex"></div>
+    </div>
+
+    <div
+      class="relative overflow-x-auto shadow-md sm:rounded-lg w-[55vw] h-[70vh] p-6 bg-white"
+    >
+      <div class="flex w-full h-[5vh] items-center mb-4">
+        <div class="w-[50%] flex">
+          <form @submit.prevent="searchStudent">
+            <div class="border bg-slate-200 px-2 rounded">
+              <q-icon class="las la-search text-gray-500" size="32px"></q-icon>
+              <input
+                type="text"
+                placeholder="Search student"
+                class="w-80 h-10 bg-slate-200 p-3 rounded-l text-gray-700 focus:outline-none focus:shadow-outline"
+                v-model="search"
+              />
+            </div>
+          </form>
+        </div>
+
+        <div class="w-[50%] flex justify-end">
+          <div class="relative inline-block text-left">
+            <div>
+              <button
+                type="button"
+                class="inline-flex w-full justify-center gap-x-1.5 rounded-md bg-white px-3 py-2 text-sm font-semibold text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 hover:bg-gray-50"
+                id="menu-button"
+                aria-expanded="true"
+                aria-haspopup="true"
+                @click="toggleDropDown"
+              >
+                Filter: {{ filterOption }}
+              </button>
+            </div>
+
+            <div
+              class="absolute right-0 z-10 mt-2 w-56 origin-top-right rounded-md bg-white shadow-lg ring-1 ring-black ring-opacity-5 focus:outline-none"
+              role="menu"
+              aria-orientation="vertical"
+              aria-labelledby="menu-button"
+              tabindex="-1"
+              v-if="sortState"
+            >
+              <div class="py-1" role="none">
+                <!-- Active: "bg-gray-100 text-gray-900", Not Active: "text-gray-700" -->
+                <a
+                  href="#"
+                  class="text-gray-700 block px-4 py-2 text-sm"
+                  role="menuitem"
+                  tabindex="-1"
+                  id="menu-item-0"
+                  @click="changeFilter('Needly Adult')"
+                  >Needly Adult
+                </a>
+                <a
+                  href="#"
+                  class="text-gray-700 block px-4 py-2 text-sm"
+                  role="menuitem"
+                  tabindex="-1"
+                  id="menu-item-1"
+                  @click="changeFilter('Survivor')"
+                  >Survivor
+                </a>
+                <a
+                  href="#"
+                  class="text-gray-700 block px-4 py-2 text-sm"
+                  role="menuitem"
+                  tabindex="-1"
+                  id="menu-item-2"
+                  @click="changeFilter('Needly Youth')"
+                  >Needly Youth
+                </a>
+                <a
+                  href="#"
+                  class="text-gray-700 block px-4 py-2 text-sm"
+                  role="menuitem"
+                  tabindex="-1"
+                  id="menu-item-2"
+                  @click="changeFilter('Needly Senior Citizen')"
+                  >Needly Senior Citizen
+                </a>
+                <a
+                  href="#"
+                  class="text-gray-700 block px-4 py-2 text-sm"
+                  role="menuitem"
+                  tabindex="-1"
+                  id="menu-item-2"
+                  @click="changeFilter('CICL')"
+                  >CICL
+                </a>
+
+                <a
+                  href="#"
+                  class="text-gray-700 block px-4 py-2 text-sm"
+                  role="menuitem"
+                  tabindex="-1"
+                  id="menu-item-2"
+                  @click="changeFilter('CEDC')"
+                  >CEDC
+                </a>
+
+                <a
+                  href="#"
+                  class="text-gray-700 block px-4 py-2 text-sm"
+                  role="menuitem"
+                  tabindex="-1"
+                  id="menu-item-2"
+                  @click="changeFilter('C/PWD')"
+                  >C/PWD
+                </a>
+
+                <a
+                  href="#"
+                  class="text-gray-700 block px-4 py-2 text-sm"
+                  role="menuitem"
+                  tabindex="-1"
+                  id="menu-item-2"
+                  @click="changeFilter('WEDC')"
+                  >WEDC
+                </a>
+                <a
+                  href="#"
+                  class="text-gray-700 block px-4 py-2 text-sm"
+                  role="menuitem"
+                  tabindex="-1"
+                  id="menu-item-2"
+                  @click="changeFilter('None')"
+                  >None
+                </a>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+      <div class="flex h-[88%]">
+        <q-table
+          flat
+          bordered
+          title="Students"
+          :rows="returnFiltered"
+          :columns="columns"
+          row-key="id"
+          :pagination="initialPagination"
+          class="flex-1"
+          @row-click="viewRow"
+        />
+      </div>
+    </div>
+
+    <div
+      class="shadow-md sm:rounded-lg flex-1 h-[40vh] p-6 bg-white ml-4"
+    >
+    <div class="flex flex-row items-center mx-10">
+      <q-icon
+                  name="las la-print"
+                  size="222px"
+                  class="text-black mx-20 my-6"
+                ></q-icon>
+    </div>
+    <div>
+
+     <button class="px-4 py-2 bg-primaryRed text-white text-base font-medium rounded-md w-full shadow-sm hover:bg-primaryRedHover focus:outline-none focus:ring-2 focus:ring-green-300"
+     data-te-toggle="modal"
+  data-te-target="#staticBackdrop"
+  data-te-ripple-init
+  data-te-ripple-color="light"
+     >Generate Report</button>
+    </div>
+  </div>
+  </div>
+</template>
+
+<script>
+import { computed, ref, onMounted } from "vue";
+import { loadDashboard } from "../services/services";
+import { useRoute, useRouter } from "vue-router";
+
+export default {
+  props: {
+    userName: {
+      type: String,
+    },
+  },
+
+  setup(props) {
+    const rows = ref([]);
+    const route = useRoute();
+    const router = useRouter();
+    onMounted(async () => {
+      rows.value = await getData();
+      filterTable();
+    });
+
+    const showSortMenu = ref(false);
+    const filterOption = ref("None");
+    const filteredArr = ref([]);
+    const search = ref("");
+    const selectedRow = ref([]);
+
+    const options = [
+      "Survivor",
+      "Needly Youth",
+      "Needly Adult",
+      "Needly Senior Citizen",
+      "CICL",
+      "CEDC",
+      "WEDC",
+      "C/PWD",
+      "None",
+    ];
+
+    const columns = [
+      {
+        name: "fullName",
+        label: "Name",
+        field: (row) => row.fullName,
+        format: (val) => `${val}`,
+        align: "left",
+
+        sortable: true,
+      },
+      {
+        name: "age",
+        label: "Age",
+        field: "age",
+        sortable: true,
+      },
+      {
+        name: "sex",
+        label: "Sex",
+        field: (row) => row.sex,
+        format: (val) => `${val}`,
+
+        sortable: true,
+      },
+      {
+        name: "category",
+        label: "Category",
+        field: (row) => row.category,
+        format: (val) => `${val}`,
+      },
+      {
+        name: "educAttn",
+        label: "Educational Attainment",
+        field: (row) => row.educAttn,
+        format: (val) => `${val}`,
+
+        sortable: true,
+      },
+      {
+        name: "status",
+        label: "Status",
+        field: (row) => row.status,
+        format: (val) => `${val}`,
+        sortable: true,
+      },
+    ];
+
+    function searchStudent() {
+      filterTable(true);
+    }
+
+    async function getData() {
+      const data = await loadDashboard();
+      return data;
+    }
+
+    function toggleDropDown() {
+      showSortMenu.value = !showSortMenu.value;
+    }
+
+    function changeFilter(filter) {
+      filterOption.value = filter;
+      filterTable();
+      toggleDropDown();
+    }
+
+    function viewRow(evt, row) {
+      const id = row.id;
+      console.log(id);
+      router.push({
+        name: "View Student Info",
+        params: { id: id },
+      });
+    }
+
+    function filterTable(isSearch = false) {
+      if (isSearch == true) {
+        const tempResults = [];
+        for (const row of rows.value) {
+          if (row.fullName.includes(search.value)) {
+            tempResults.push(row);
+          }
+        }
+        filteredArr.value = tempResults;
+
+        return filteredArr;
+      } else {
+        for (const option of options) {
+          if (filterStr.value === "None") {
+            filteredArr.value = rows.value;
+            return filteredArr.value;
+          }
+
+          if (filterStr.value === option) {
+            filteredArr.value = rows.value.filter(
+              (row) => row.category == filterStr.value
+            );
+            return filteredArr.value;
+          }
+        }
+      }
+    }
+
+    const returnFiltered = computed(() => {
+      return filteredArr.value;
+    });
+
+    const sortState = computed(() => {
+      return showSortMenu.value;
+    });
+
+    const filterStr = computed(() => {
+      return filterOption.value;
+    });
+
+    return {
+      initialPagination: {
+        sortBy: "name",
+        descending: false,
+        page: 1,
+        rowsPerPage: 5,
+        // rowsNumber: xx if getting data from a server
+      },
+      props,
+      toggleDropDown,
+      sortState,
+      rows,
+      columns,
+      filterOption,
+      changeFilter,
+      filterTable,
+      filterStr,
+      search,
+      searchStudent,
+      returnFiltered,
+      selectedRow,
+      viewRow,
     };
   },
 };

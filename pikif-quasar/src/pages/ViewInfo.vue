c12344a163e8c2051b0d3f56ac123dec654fc007
MarkLumbay <20180013531@my.xu.edu.ph>
2023-05-19 00:03:11 +0800

    fixed ui for view stud info

--- pikif-quasar/src/pages/ViewInfo.vue
+++ pikif-quasar/src/pages/ViewInfo.vue
@@ -1,55 +1,57 @@
 <template>
-  <div
-    class="w-[90%] flex flex-col shadow-md rounded-xl p-4 space-x-4 bg-white"
-    v-if="!showMsg"
-  >
-    <div>
-      <AddClient
-        :withProp="prop"
-        @client-info-update="saveUpdatedClientInfo"
-      ></AddClient>
-    </div>
-  </div>
-
-  <div
-    class="w-[90%] flex flex-col shadow-md rounded-xl p-4 space-x-4 bg-white mt-8"
-    v-if="!showMsg"
-  >
-    <div>
-      <AddInformant
-        :withProp="prop"
-        @informant-info-update="saveUpdatedInformantInfo"
-      ></AddInformant>
-    </div>
-  </div>
-
-  <div
-    class="w-[90%] flex flex-col shadow-md rounded-xl p-4 space-x-4 bg-white mt-8"
-    v-if="!showMsg"
-  >
-    <div>
-      <AddFindings
-        :withProp="prop"
-        @client-findings-update="saveUpdatedClientFindings"
-        @new-client-findings="addNewFindings"
-      ></AddFindings>
+  <div class="px-6 py-8 flex items-center justify-center h-full">
+    <div
+      class="w-[90%] flex flex-col shadow-md rounded-xl p-4 space-x-4 bg-white"
+      v-if="!showMsg"
+    >
+      <div>
+        <AddClient
+          :withProp="prop"
+          @client-info-update="saveUpdatedClientInfo"
+        ></AddClient>
+      </div>
     </div>
 
-    <div class="flex w-full justify-end space-x-4 mt-4 px-3">
-      <button
-        class="bg-primaryRed mb-2 w-[12vw] hover:bg-primaryRedHover text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline h-14 transition-all ease-in-out"
-        @click.prevent="updateData"
-      >
-        Save Update
-      </button>
+    <div
+      class="w-[90%] flex flex-col shadow-md rounded-xl p-4 space-x-4 bg-white mt-8"
+      v-if="!showMsg"
+    >
+      <div>
+        <AddInformant
+          :withProp="prop"
+          @informant-info-update="saveUpdatedInformantInfo"
+        ></AddInformant>
+      </div>
     </div>
-  </div>
 
-  <div
-    class="w-[90%] flex flex-col shadow-md rounded-xl p-4 space-x-4 bg-white"
-    v-if="showMsg"
-  >
-    <MessagePage :message="message"></MessagePage>
+    <div
+      class="w-[90%] flex flex-col shadow-md rounded-xl p-4 space-x-4 bg-white mt-8"
+      v-if="!showMsg"
+    >
+      <div>
+        <AddFindings
+          :withProp="prop"
+          @client-findings-update="saveUpdatedClientFindings"
+          @new-client-findings="addNewFindings"
+        ></AddFindings>
+      </div>
+
+      <div class="flex w-full justify-end space-x-4 mt-4 px-3">
+        <button
+          class="bg-primaryRed mb-2 w-[12vw] hover:bg-primaryRedHover text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline h-14 transition-all ease-in-out"
+          @click.prevent="updateData"
+        >
+          Save Update
+        </button>
+      </div>
+    </div>
+
+    <div
+      class="w-[90%] flex flex-col shadow-md rounded-xl p-4 space-x-4 bg-white"
+      v-if="showMsg"
+    >
+      <MessagePage :message="message"></MessagePage>
+    </div>
   </div>
 </template>
 

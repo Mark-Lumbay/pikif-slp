1965deca5f502f29768dc723f84f32cae845ce1c
MarkLumbay <20180013531@my.xu.edu.ph>
2023-05-20 01:01:43 +0800

    revised ui for notice modal

--- pikif-quasar/src/components/AlertBox.vue
+++ pikif-quasar/src/components/AlertBox.vue
@@ -2,17 +2,25 @@
   <div>
     <q-dialog v-model="alert" persistent>
       <q-card>
-        <q-card-section>
-          <div class="text-h6">Notice</div>
+        <q-card-section class="w-full bg-primaryYellow">
+          <div class="w-full flex space-x-2">
+            <q-icon name="las la-exclamation-triangle" size="35px"></q-icon>
+            <p class="text-2xl font-semibold">Notice</p>
+          </div>
         </q-card-section>
 
-        <q-card-section class="q-pt-none">
+        <q-card-section class="text-base mt-2">
           You lack the access previliges to perform this action. If you think
           that this is a mistake, please contact an administrator.
         </q-card-section>
 
-        <q-card-actions align="right">
-          <q-btn flat label="OK" color="primary" @click="toggle" />
+        <q-card-actions align="right" class="mb-2 mr-2">
+          <button
+            class="text-primaryBtn h-12 hover:text-white hover:bg-primaryHovBtn hover:border-transparent font-semibold py-2 px-6 border border-primaryBtn rounded"
+            @click="toggle"
+          >
+            OK
+          </button>
         </q-card-actions>
       </q-card>
     </q-dialog>

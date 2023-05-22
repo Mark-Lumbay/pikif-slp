c1b7b39f540ae5f3ff5a9d8c7a4364a6b84b3459
MarkLumbay <20180013531@my.xu.edu.ph>
2023-05-22 14:01:28 +0800

    created admin dashboard file

---
+++ pikif-quasar/src/pages/AdminDashboard.vue
@@ -0,0 +1,31 @@
+<template>
+  <div class="flex justify-center items-center">
+    <div class="w-[88%] flex mt-8">
+      <q-table
+        bordered
+        title="Audit Log"
+        :rows="rows"
+        :columns="columns"
+        row-key="id"
+        :pagination="initialPagination"
+        class="w-full"
+      />
+    </div>
+  </div>
+</template>
+
+<script>
+import { ref, onMounted } from "vue";
+
+export default {
+  setup() {
+    const rows = ref([]);
+    const columns = [];
+
+    return {
+      rows,
+      columns,
+    };
+  },
+};
+</script>

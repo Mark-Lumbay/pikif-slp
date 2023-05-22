da953b05991bb44f591a9672ede21e01df763d70
MarkLumbay <20180013531@my.xu.edu.ph>
2023-05-22 15:28:28 +0800

    added dropdown for role column

--- pikif-quasar/src/pages/AdminDashboard.vue
+++ pikif-quasar/src/pages/AdminDashboard.vue
@@ -3,13 +3,24 @@
     <div class="w-[88%] flex mt-8">
       <q-table
         bordered
-        title="Audit Log"
+        title="Accounts Management"
         :rows="rows"
         :columns="columns"
         row-key="id"
         :pagination="initialPagination"
         class="w-full"
-      />
+      >
+        <template v-slot:body-cell-role="props">
+          <q-td key="id" :props="props" class="">
+            <q-select
+              filled
+              v-model="props.row.role"
+              :options="roleRef"
+              @update:model-value="changeRole(props.row.role, props.row.id)"
+            />
+          </q-td>
+        </template>
+      </q-table>
     </div>
   </div>
 </template>
@@ -21,6 +32,12 @@ import { getAccessLvl } from "../services/services";
 export default {
   setup() {
     const rows = ref([]);
+    const selectedRole = ref("");
+    const roleRef = ref([
+      "Teacher",
+      "Social Development Worker",
+      "Administrator",
+    ]);
     const columns = [
       {
         name: "id",
@@ -41,12 +58,17 @@ export default {
       {
         name: "role",
         label: "Role",
+        selected: (row) => row.role,
         field: (row) => row.role,
         format: (val) => `${val}`,
         align: "left",
         sortable: false,
       },
     ];
+
+    const changeRole = (newRole, id) => {
+      console.log(newRole, id);
+    };
 
     onMounted(async () => {
       rows.value = await getAccessLvl();
@@ -56,6 +78,9 @@ export default {
     return {
       rows,
       columns,
+      roleRef,
+      selectedRole,
+      changeRole,
     };
   },
 };

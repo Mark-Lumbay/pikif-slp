a06d6e58471771d45dcc9d1facaa5fb764950682
MarkLumbay <20180013531@my.xu.edu.ph>
2023-05-23 21:46:31 +0800

    changed another endpoint

--- pikif-quasar/src/boot/axios.js
+++ pikif-quasar/src/boot/axios.js
@@ -8,7 +8,7 @@ import axios from "axios";
 // "export default () => {}" function below (which runs individually
 // for each client)
 const api = axios.create({
-  baseURL: "https://pikif-slp-production.up.railway.app",
+  baseURL: "pikif-slp-production.up.railway.app/island-kids",
 });
 
 export default boot(({ app }) => {
